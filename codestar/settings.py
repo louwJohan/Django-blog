4f21a81e4f865fcd4a684905c98b47b7a4aada75
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-05-11 18:25:55 +0000

    Deloyment

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-blog-codestar-johan.herokuapp.com', 'localhost']
 
@@ -59,6 +59,9 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+ 
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
